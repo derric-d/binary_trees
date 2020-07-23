d24c5d97e562b80d39957f5f42f755872e776f91
derric-d <derric.donehoo@gmail.com>
2020-07-23 18:06:11 +0000

    typo

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,4 +1,4 @@
-#incldue "binary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_is_leaf - check if has no children
@@ -12,4 +12,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 
 	if (!node->left && !node->right)
 		return (1);
+
+	return (0);
 }
